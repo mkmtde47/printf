2304debf7d8d6de55e912a2dd89b840933f9bd1a
mkmtde47 <mkmmogano@gmail.com>
2023-04-24 11:37:00 -0700

    7 returns

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+int main() {
+    char *str1 = NULL;
+    char *str2 = "";
+    _printf("Printing a null pointer: %s\n", str1);
+    _printf("Printing an empty string: %s\n", str2);
+    return 0;
+}
+
